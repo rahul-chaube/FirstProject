95269bad4183f9010f520ec32f099787f6df602e
EC2 Default User <ec2-user@ip-172-31-5-38.ap-south-1.compute.internal>
2023-10-31 07:04:43 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	iris "github.com/kataras/iris/v12"
+)
+
+func main() {
+	app := iris.New()
+
+	user := app.Party("/user")
+	{
+		user.Get("/", list)
+		user.Post("/", create)
+	}
+	app.Listen(":8080")
+}
+
+func list(ctx iris.Context) {
+	ctx.Write([]byte("Hellow world "))
+
+}
+func create(ctx iris.Context) {
+	ctx.Write([]byte("Post Request is called "))
+
+}
